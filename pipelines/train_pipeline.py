f01b7b248a0856a5b8b40bb116d6c3e23f7908a7
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-11-22 15:44:27 -0300

    feat: ajustando treinamento do densenet

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -13,23 +13,30 @@ from torchvision.models import densenet121, DenseNet121_Weights
 from torchvision import transforms
 from tempfile import TemporaryDirectory
 
-device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+device = (
+    torch.accelerator.current_accelerator().type
+    if torch.accelerator.is_available()
+    else "cpu"
+)
 print(f"Using {device} device")
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=100):
+
+def train_model(
+    model, dataloaders, dataset_sizes, criterion, optimizer, num_epochs=100
+):
     since = time.time()
 
     with TemporaryDirectory() as tempdir:
-        best_model_params_path = os.path.join(tempdir, 'best_model_params.pt')
+        best_model_params_path = os.path.join(tempdir, "best_model_params.pt")
         torch.save(model.state_dict(), best_model_params_path)
         best_acc = 0.0
 
         for epoch in range(num_epochs):
-            print(f'Epoch {epoch}/{num_epochs-1}')
-            print('-'*10)
+            print(f"Epoch {epoch}/{num_epochs - 1}")
+            print("-" * 10)
 
-            for phase in ['train', 'val']:
-                if phase == 'train':
+            for phase in ["train", "val"]:
+                if phase == "train":
                     model.train()
                 else:
                     model.eval()
@@ -43,36 +50,40 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=100):
 
                     optimizer.zero_grad()
 
-                    with torch.set_grad_enabled(phase == 'train'):
+                    with torch.set_grad_enabled(phase == "train"):
                         outputs = model(inputs)
                         _, preds = torch.max(outputs, 1)
                         loss = criterion(outputs, labels)
 
-                        if phase == 'train':
+                        if phase == "train":
                             loss.backward()
                             optimizer.step()
 
                     running_loss += loss.item() * inputs.size(0)
                     running_corrects += torch.sum(preds == labels.data)
-                
+
                 epoch_loss = running_loss / dataset_sizes[phase]
                 epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+                print(f"{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}")
 
-                if phase == 'val' and epoch_acc > best_acc:
+                if phase == "val" and epoch_acc > best_acc:
                     best_acc = epoch_acc
                     torch.save(model.state_dict(), best_model_params_path)
 
             print()
 
         time_elapsed = time.time() - since
-        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f'Best val acc: {best_acc:4f}')
+        print(
+            f"Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s"
+        )
+        print(f"Best val acc: {best_acc:4f}")
 
         model.load_state_dict(torch.load(best_model_params_path, weights_only=True))
+
     return model
-            
+
+
 def unfreeze_layers(model, n_layers):
     modules = [m for m in model.modules() if len(list(m.parameters())) > 0]
     modules_to_train = modules[-n_layers:]
@@ -80,34 +91,84 @@ def unfreeze_layers(model, n_layers):
         for param in module.parameters():
             param.requires_grad = True
 
+
 def train_resnet(dataset_path):
     print("Train ResNet")
+
 
 def train_densenet(dataset_path, layers, kfolds, batch_size, epochs):
     print("Train DenseNet")
 
     weights = DenseNet121_Weights.IMAGENET1K_V1
     model = densenet121(weights=weights)
-    
+
     for param in model.parameters():
-            param.requires_grad = False
+        param.requires_grad = False
 
     unfreeze_layers(model, layers)
 
     num_ftrs = model.classifier.in_features
     model.classifier = nn.Linear(num_ftrs, 2)
-    
+
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4)
-    transform = weights.transforms 
+    optimizer = optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()), lr=1e-4
+    )
+    transform = weights.transforms
+
+    data = load_data(dataset_path, n_splits=kfolds, transform=transform)
+
+    for fold in data.keys():
+        print(f"Training fold {fold + 1} of {kfolds}")
+
+        fold_data = data[fold]
+
+        X_train = fold_data["X_train"]
+        y_train = fold_data["y_train"]
+        X_val = fold_data["X_val"]
+        y_val = fold_data["y_val"]
+
+        train_sampler = BatchSampler(y_train, batch_size)
+        train_loader = DataLoader(
+            X_train,
+            sampler=train_sampler,
+            pin_memory=True,
+            num_workers=4,
+        )
+        val_loader = DataLoader(
+            X_val,
+            batch_size=batch_size,
+            pin_memory=True,
+            num_workers=4,
+        )
+
+        dataloaders = {
+            "train": train_loader,
+            "val": val_loader,
+        }
+
+        dataset_sizes = {
+            "train": len(y_train),
+            "val": len(y_val),
+        }
+
+        model = train_model(
+            model,
+            dataloaders,
+            dataset_sizes,
+            criterion,
+            optimizer,
+            epochs,
+        )
+
 
 def run(dataset_path, pretrained_model, trainable_layers, kfolds, batch_size, epochs):
-    if (pretrained_model == "densenet"):
+    if pretrained_model == "densenet":
         train_densenet(dataset_path, trainable_layers, kfolds, batch_size, epochs)
         return
-    
-    if (pretrained_model == "resnet"):
+
+    if pretrained_model == "resnet":
         train_resnet(dataset_path)
         return
--- utils/data_loader.py
+++ utils/data_loader.py
@@ -12,11 +12,12 @@ from sklearn.model_selection import StratifiedKFold
 from torchvision.io import decode_image
 from torch.utils.data import DataLoader, Dataset, Subset, Sampler
 
+
 class CustomDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.df = self.gen_dataframe(img_dir)
-        self.img_labels = self.df['idx']
-        self.img_dir = self.df['path']
+        self.img_labels = self.df["idx"]
+        self.img_dir = self.df["path"]
         self.transform = transform
 
     def __len__(self):
@@ -31,32 +32,33 @@ class CustomDataset(Dataset):
         return image, label
 
     def gen_dataframe(self, path):
-        map_result = { "path": [], "name": [], "class": [], "idx": [] }
+        map_result = {"path": [], "name": [], "class": [], "idx": []}
         class_names = os.listdir(path)
         for name in class_names:
             class_path = os.path.join(path, f"{name}/")
             for img in os.listdir(class_path):
                 if not img.endswith(".png"):
                     continue
-                map_result['path'].append(os.path.join(class_path, img))
-                map_result['name'].append(img)
-                map_result['class'].append(name)
-                map_result['idx'].append(0 if name == "NORMAL" else 1)
+                map_result["path"].append(os.path.join(class_path, img))
+                map_result["name"].append(img)
+                map_result["class"].append(name)
+                map_result["idx"].append(0 if name == "NORMAL" else 1)
         dataframe = pd.DataFrame(data=map_result)
         return dataframe
-            
+
+
 class CustomSampler(Sampler):
     def __init__(self, labels, batch_size=32):
         self.labels = labels
         self.classes = np.unique(self.labels)
         self.N = len(self.classes)
         self.m_per_class = batch_size // self.N
-        
+
         self.S = {c: np.where(self.labels == c)[0].tolist() for c in self.classes}
         self.C = {c: len(self.S[c]) for c in self.classes}
         self.c_max = max(self.C.values())
         self.K = self.c_max // self.m_per_class
-        
+
     def __len__(self):
         return self.K
 
@@ -72,17 +74,18 @@ class CustomSampler(Sampler):
                 if len(S_work[c]) < self.m_per_class:
                     S_work[c] = list(self.S[c])
                     np.random.shuffle(S_work[c])
-                chosen = S_work[c][:self.m_per_class]
-                S_work[c] = S_work[c][self.m_per_class:]
+                chosen = S_work[c][: self.m_per_class]
+                S_work[c] = S_work[c][self.m_per_class :]
                 batch.extend(chosen)
             yield batch
+
 
 def load_data(path, n_splits=5, transform=None):
     full_dataset = CustomDataset(path, transform=transform)
     labels = full_dataset.img_labels
-    
+
     kf = StratifiedKFold(n_splits=n_splits)
-    
+
     map_result = {}
 
     for fold, (train_idx, val_idx) in enumerate(kf.split(full_dataset.df, y=labels)):
@@ -91,18 +94,18 @@ def load_data(path, n_splits=5, transform=None):
         train_labels = labels[train_idx]
         val_labels = labels[val_idx]
 
-        map_result[fold] = {"X_train": train_dataset, "y_train": train_labels, "X_val": val_dataset, "y_val": val_labels}
+        map_result[fold] = {
+            "X_train": train_dataset,
+            "y_train": train_labels,
+            "X_val": val_dataset,
+            "y_val": val_labels,
+        }
 
     return map_result
+
 
 if __name__ == "__main__":
     path = "../../../datasets/CXR8/train/"
     data = load_data(path, n_splits=5)
     for fold in data.keys():
         print(data[fold]["y_train"].value_counts())
-
-
-
-
-
-    
