542d154a98b08b1dad4349229630c392d1694ff2
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-11-23 07:05:14 -0300

    feat: ajustando escolhas

--- main.py
+++ main.py
@@ -8,48 +8,41 @@ from pipelines import train_pipeline
 parser = argparse.ArgumentParser(prog="Medical Imaging Analysis Classifier")
 
 parser.add_argument(
-        '--model',
-        choices=('resnet', 'mobilenet', 'densenet', 'efficientnet', 'vit'),
-        help='Pretrained Model',
-        default='densenet')
+    "--model",
+    choices=("resnet", "mobilenet", "densenet", "efficientnet", "vit"),
+    help="Pretrained Model",
+    default="densenet",
+)
 
 parser.add_argument(
-        '--layers',
-        type=int,
-        choices=('5', '10', '15', '20', '25'),
-        help='Trainable Layers',
-        default='10')
+    "--layers",
+    type=int,
+    choices=("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"),
+    help="Trainable Layers",
+    default="5",
+)
 
 parser.add_argument(
-        '--folds',
-        type=int,
-        choices=('3', '5', '7', '10'),
-        help="KFolds Qtd",
-        default='5')
+    "--folds", type=int, choices=("3", "5", "7", "10"), help="KFolds Qtd", default="5"
+)
+
+parser.add_argument("--batch-size", type=int, default="32", help="Batch Size")
 
 parser.add_argument(
-        '--batch-size',
-        type=int,
-        default='32',
-        help='Batch Size')
+    "--dataset", choices=("chest_xray", "CXR8"), help="Training Dataset", default="CXR8"
+)
 
-parser.add_argument(
-        '--dataset',
-        choices=('chest_xray', 'CXR8'),
-        help='Training Dataset',
-        default='CXR8')
-
-parser.add_argument(
-        '--epochs',
-        type=int,
-        default='100',
-        help='Training Epochs')
+parser.add_argument("--epochs", type=int, default="100", help="Training Epochs")
 
 args = parser.parse_args()
 
+
 def main():
     dataset_path = os.path.join("../../datasets", args.dataset)
-    train_pipeline(dataset_path, args.model, args.layers, args.folds, args.batch_size, args.epochs)
+    train_pipeline(
+        dataset_path, args.model, args.layers, args.folds, args.batch_size, args.epochs
+    )
+
 
 if __name__ == "__main__":
     main()
