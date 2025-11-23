8d5286d479ae85336680c49e3428ea7d383466be
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-11-23 18:33:30 -0300

    minor: refazendo testes

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -8,6 +8,7 @@ from custom_dataset import CustomDataset
 from sklearn.model_selection import StratifiedKFold, train_test_split
 from torch.utils.data import DataLoader
 from custom_sampler import CustomSampler
+from torchvision import transforms
 
 
 def gen_dataframe(root_dir):
@@ -78,82 +79,18 @@ def load_data(path, n_splits=None, transform=None, val_transform=None, training=
             }
 
 
-def visualise_dataloader(dl, id_to_label=None):
-    total_num_images = len(dl.dataset)
-    idxs_seen = []
-    class_0_batch_counts = []
-    class_1_batch_counts = []
-
-    for i, batch in enumerate(dl):
-        idxs = batch[0][:, 0].tolist()
-        classes = batch[0][:, 1]
-        class_ids, class_counts = classes.unique(return_counts=True)
-        class_ids = set(class_ids.tolist())
-        class_counts = class_counts.tolist()
-
-        idxs_seen.extend(idxs)
-
-        print(class_ids)
-        print(class_counts)
-
-        if len(class_ids) == 2:
-            class_0_batch_counts.append(class_counts[0])
-            class_1_batch_counts.append(class_counts[1])
-        elif len(class_ids) == 1 and 0 in class_ids:
-            class_0_batch_counts.append(class_counts[0])
-            class_1_batch_counts.append(0)
-        elif len(class_ids) == 1 and 1 in class_ids:
-            class_0_batch_counts.append(0)
-            class_1_batch_counts.append(class_counts[0])
-        else:
-            raise ValueError("More than two classes detected")
-    return class_0_batch_counts, class_1_batch_counts, idxs_seen, total_num_images
-
-
 if __name__ == "__main__":
     path = "../../../datasets/CXR8/train/"
-    data = load_data(path, n_splits=5)
-    for fold in data.keys():
-        print(f"Fold {fold + 1}")
-        print("Train:")
-        print(pd.DataFrame(data=data[fold]["y_train"]).value_counts(normalize=True))
-        print("Val:")
-        print(pd.DataFrame(data=data[fold]["y_val"]).value_counts(normalize=True))
-        print("-" * 10)
-        batch_sampler = CustomSampler(data[fold]["y_train"])
-        dataloader = DataLoader(
-            data[fold]["X_train"],
-            batch_sampler=batch_sampler,
-            pin_memory=True,
-            num_workers=4,
-        )
-        class_0, class_1, idxs_seen, total_num_images = (
-            visualise_dataloader(dataloader, {0: "NORMAL", 1: "PNEUMONIA"}),
-        )
-        print(f"Class 0: {class_0}")
-        print(f"Class 1: {class_1}")
-        print(f"Total number of images: {total_num_images}")
-        print(f"Idxs seen: {idxs_seen}")
-
-    print("-" * 10)
-    data = load_data(path, n_splits=None)
-    print("All data:")
-    print("Train:")
-    print(pd.DataFrame(data=data[0]["y_train"]).value_counts(normalize=True))
-    print("Val:")
-    print(pd.DataFrame(data=data[0]["y_val"]).value_counts(normalize=True))
-    print("-" * 10)
-    batch_sampler = CustomSampler(data[0]["y_train"])
-    dataloader = DataLoader(
-        data[0]["X_train"],
-        batch_sampler=batch_sampler,
-        pin_memory=True,
-        num_workers=4,
+    data = load_data(
+        path,
+        n_splits=None,
+        transform=transforms.ToTensor(),
+        val_transform=transforms.ToTensor(),
     )
-    class_0, class_1, idxs_seen, total_num_images = (
-        visualise_dataloader(dataloader, {0: "NORMAL", 1: "PNEUMONIA"}),
-    )
-    print(f"Class 0: {class_0}")
-    print(f"Class 1: {class_1}")
-    print(f"Total number of images: {total_num_images}")
-    print(f"Idxs seen: {idxs_seen}")
+    dt = data[0]["X_train"]
+    print(dt.labels)
+    sampler = CustomSampler(data[0]["y_train"], batch_size=32)
+    dl = DataLoader(dt, batch_sampler=sampler)
+    for batch in dl:
+        print(batch)
+        break
