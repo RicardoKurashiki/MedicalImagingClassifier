a950b6157a4ea6286a8b00430f1af5093b847666
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-11-23 18:17:54 -0300

    feat: adicionado pontos de debug do batch sampler

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -6,6 +6,8 @@ import numpy as np
 
 from utils.custom_dataset import CustomDataset
 from sklearn.model_selection import StratifiedKFold, train_test_split
+from torch.utils.data import DataLoader
+from utils.custom_sampler import CustomSampler
 
 
 def gen_dataframe(root_dir):
@@ -30,7 +32,13 @@ def load_data(path, n_splits=None, transform=None, val_transform=None, training=
     if not training:
         test_dataset = CustomDataset(df, transform=transform)
         test_labels = df["label"].values
-        return {0: {"X_test": test_dataset, "y_test": test_labels}}
+        return {
+            0: {
+                "test_df": df,
+                "X_test": test_dataset,
+                "y_test": test_labels,
+            }
+        }
     else:
         if n_splits is not None and n_splits > 0:
             kf = StratifiedKFold(n_splits=n_splits, random_state=42, shuffle=True)
@@ -44,6 +52,8 @@ def load_data(path, n_splits=None, transform=None, val_transform=None, training=
                 val_labels = df_val["label"].values
 
                 map_result[fold] = {
+                    "train_df": df_train,
+                    "val_df": df_val,
                     "X_train": train_dataset,
                     "y_train": train_labels,
                     "X_val": val_dataset,
@@ -58,6 +68,8 @@ def load_data(path, n_splits=None, transform=None, val_transform=None, training=
             val_labels = val_df["label"].values
             return {
                 0: {
+                    "train_df": train_df,
+                    "val_df": val_df,
                     "X_train": train_dataset,
                     "y_train": train_labels,
                     "X_val": val_dataset,
@@ -66,8 +78,79 @@ def load_data(path, n_splits=None, transform=None, val_transform=None, training=
             }
 
 
+def visualise_dataloader(dl, id_to_label=None):
+    total_num_images = len(dl.dataset)
+    idxs_seen = []
+    class_0_batch_counts = []
+    class_1_batch_counts = []
+
+    for i, batch in enumerate(dl):
+        idxs = batch[0][:, 0].tolist()
+        classes = batch[0][:, 1]
+        class_ids, class_counts = classes.unique(return_counts=True)
+        class_ids = set(class_ids.tolist())
+        class_counts = class_counts.tolist()
+
+        idxs_seen.extend(idxs)
+
+        if len(class_ids) == 2:
+            class_0_batch_counts.append(class_counts[0])
+            class_1_batch_counts.append(class_counts[1])
+        elif len(class_ids) == 1 and 0 in class_ids:
+            class_0_batch_counts.append(class_counts[0])
+            class_1_batch_counts.append(0)
+        elif len(class_ids) == 1 and 1 in class_ids:
+            class_0_batch_counts.append(0)
+            class_1_batch_counts.append(class_counts[0])
+        else:
+            raise ValueError("More than two classes detected")
+    return class_0_batch_counts, class_1_batch_counts, idxs_seen, total_num_images
+
+
 if __name__ == "__main__":
     path = "../../../datasets/CXR8/train/"
     data = load_data(path, n_splits=5)
     for fold in data.keys():
-        print(data[fold]["y_train"].value_counts())
+        print(f"Fold {fold + 1}")
+        print("Train:")
+        print(data[fold]["y_train"].value_counts(normalize=True))
+        print("Val:")
+        print(data[fold]["y_val"].value_counts(normalize=True))
+        print("-" * 10)
+        batch_sampler = CustomSampler(data[fold]["y_train"])
+        dataloader = DataLoader(
+            data[fold]["X_train"],
+            batch_sampler=batch_sampler,
+            pin_memory=True,
+            num_workers=4,
+        )
+        class_0, class_1, idxs_seen, total_num_images = (
+            visualise_dataloader(dataloader, {0: "NORMAL", 1: "PNEUMONIA"}),
+        )
+        print(f"Class 0: {class_0}")
+        print(f"Class 1: {class_1}")
+        print(f"Total number of images: {total_num_images}")
+        print(f"Idxs seen: {idxs_seen}")
+
+    print("-" * 10)
+    data = load_data(path, n_splits=None)
+    print("All data:")
+    print("Train:")
+    print(data[0]["y_train"].value_counts(normalize=True))
+    print("Val:")
+    print(data[0]["y_val"].value_counts(normalize=True))
+    print("-" * 10)
+    batch_sampler = CustomSampler(data[0]["y_train"])
+    dataloader = DataLoader(
+        data[0]["X_train"],
+        batch_sampler=batch_sampler,
+        pin_memory=True,
+        num_workers=4,
+    )
+    class_0, class_1, idxs_seen, total_num_images = (
+        visualise_dataloader(dataloader, {0: "NORMAL", 1: "PNEUMONIA"}),
+    )
+    print(f"Class 0: {class_0}")
+    print(f"Class 1: {class_1}")
+    print(f"Total number of images: {total_num_images}")
+    print(f"Idxs seen: {idxs_seen}")
