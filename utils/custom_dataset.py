3dd8d44b7439430e304b18540f32b765de9653d3
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-11-23 18:25:16 -0300

    fix: sempre transforma imagem em tensor

--- utils/custom_dataset.py
+++ utils/custom_dataset.py
@@ -1,5 +1,6 @@
 from torch.utils.data import Dataset
 from PIL import Image
+from torchvision import transforms
 
 
 class CustomDataset(Dataset):
@@ -8,6 +9,8 @@ class CustomDataset(Dataset):
         self.img_paths = self.dataframe["path"].values
         self.labels = self.dataframe["label"].values
         self.transform = transform
+        if transform is None:
+            self.transform = transforms.ToTensor()
 
         self.unique_labels = sorted(self.dataframe["label"].unique())
         self.label_to_idx = {label: idx for idx, label in enumerate(self.unique_labels)}
@@ -15,19 +18,10 @@ class CustomDataset(Dataset):
     def __len__(self):
         return len(self.labels)
 
-    # def __getitem__(self, idx):
-    #     img_path = str(self.img_paths[idx])
-    #     image = decode_image(img_path)
-    #     label = self.labels[idx]
-    #     if self.transform:
-    #         image = self.transform(image)
-    #     return image, label
-
     def __getitem__(self, idx):
         img_path = str(self.img_paths[idx])
         image = Image.open(img_path).convert("RGB")
         label = self.labels[idx]
         label = self.label_to_idx[label]
-        if self.transform:
-            image = self.transform(image)
+        image = self.transform(image)
         return image, label
