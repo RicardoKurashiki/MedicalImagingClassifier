096c0faef6f1dea1a4ec8367536f6995b6f8164a
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-12-07 17:16:32 -0300

    feat: alterando lógica do batch sampler

--- utils/custom_sampler.py
+++ utils/custom_sampler.py
@@ -2,8 +2,6 @@ import numpy as np
 
 from torch.utils.data import Sampler
 
-np.random.seed(42)
-
 
 class CustomSampler(Sampler):
     def __init__(self, dataset, batch_size=32):
@@ -15,8 +13,8 @@ class CustomSampler(Sampler):
 
         self.S = {c: np.where(self.labels == c)[0].tolist() for c in self.classes}
         self.C = {c: len(self.S[c]) for c in self.classes}
-        self.c_max = max(self.C.values())
-        self.K = self.c_max // self.m_per_class
+        self.c_min = min(self.C.values())
+        self.K = self.c_min // self.m_per_class
 
     def __len__(self):
         return self.K
@@ -31,10 +29,16 @@ class CustomSampler(Sampler):
             batch = []
             for c in self.classes:
                 if len(S_work[c]) < self.m_per_class:
+                    remaining = S_work[c]
                     S_work[c] = list(self.S[c])
                     np.random.shuffle(S_work[c])
+                    for used_idx in remaining:
+                        if used_idx in S_work[c]:
+                            S_work[c].remove(used_idx)
+                    if len(S_work[c]) < self.m_per_class:
+                        S_work[c] = list(self.S[c])
+                        np.random.shuffle(S_work[c])
                 chosen = S_work[c][: self.m_per_class]
                 batch.extend(chosen)
-                if len(self.S[c]) == self.c_max:
-                    S_work[c] = S_work[c][self.m_per_class :]
+                S_work[c] = S_work[c][self.m_per_class :]
             yield batch
