5a6edb50abac0cee9c791c7e7c1cf60da1e88f54
Ricardo Godoi Kurashiki <ricardo.g.kurashiki@gmail.com>
2025-12-13 15:23:45 -0300

    fix: adicionado remoção de data aug

--- run_code.py
+++ run_code.py
@@ -19,29 +19,30 @@ def main():
     models = ["resnet", "densenet", "mobilenet", "efficientnet"]
     layers = [None, 2, 5]
     batch_sizes = [32]
-    datasets = [
-        "rsna",
-        "chest_xray",
-    ]
+    datasets = ["rsna", "chest_xray"]
+    data_augs = [True, False]
     epochs = 500
 
     configs = []
 
     for dataset in datasets:
-        for layer in layers:
-            for batch_size in batch_sizes:
-                for model in models:
-                    config = "python3 main.py"
-                    if model is not None:
-                        config += f" --model {model}"
-                    if layer is not None:
-                        config += f" --layers {layer}"
-                    if batch_size is not None:
-                        config += f" --batch-size {batch_size}"
-                    if epochs is not None:
-                        config += f" --epochs {epochs}"
-                    config += f" --dataset {dataset}"
-                    configs.append(config)
+        for data_aug in data_augs:
+            for layer in layers:
+                for batch_size in batch_sizes:
+                    for model in models:
+                        config = "python3 main.py"
+                        if model is not None:
+                            config += f" --model {model}"
+                        if layer is not None:
+                            config += f" --layers {layer}"
+                        if batch_size is not None:
+                            config += f" --batch-size {batch_size}"
+                        if epochs is not None:
+                            config += f" --epochs {epochs}"
+                        config += f" --dataset {dataset}"
+                        if data_aug is False:
+                            config += " --no-data-aug"
+                        configs.append(config)
 
     print(
         f"Total de configurações: {len(configs)} | Paralelas: {n_parallel} instâncias"
